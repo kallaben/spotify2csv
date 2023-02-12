1d2d60d2d6b87572d9b1c408494db351952c0806
kallaben <didironja@hotmail.com>
2023-02-12 10:56:47 +0100

    Delete node backend

--- backend/services/requestProvider.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-
-export default class RequestProvider {
-  constructor(private req: Request) {}
-
-  getRequest(): Request {
-    return this.req;
-  }
-}
--- backend/services/userContext.ts
+++
@@ -1,7 +0,0 @@
-export default class UserContext {
-  constructor(private userId: string) {}
-
-  public getId(): string {
-    return this.userId;
-  }
-}
