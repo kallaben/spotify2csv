b9b9d585678fe1f719457f7f53c12f2afd184e5f
kallaben <didironja@hotmail.com>
2023-07-05 22:07:27 +0200

    Button directs to export component when user is logged in

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RedirectComponent } from './redirect/redirect.component';
 import { HomeComponent } from './home/home.component';
+import { ExportComponent } from './export/export.component';
 
 const routes: Routes = [
   { path: 'redirect', component: RedirectComponent },
+  { path: 'export', component: ExportComponent },
   { path: '', component: HomeComponent },
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,18 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RedirectComponent } from './redirect/redirect.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ExportComponent } from './export/export.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RedirectComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RedirectComponent, HomeComponent, ExportComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
