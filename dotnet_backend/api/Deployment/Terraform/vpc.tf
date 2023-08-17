415bd2d584a86adab184c744d36ccbb5807b58ef
kallaben <didironja@hotmail.com>
2023-08-17 16:25:07 +0200

    BE: Increase subnet IP ranges

--- dotnet_backend/api/Deployment/Terraform/vpc.tf
+++ dotnet_backend/api/Deployment/Terraform/vpc.tf
@@ -25,15 +25,15 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  ip_cidr_range = "10.5.0.0/20"
 
   secondary_ip_range {
     range_name    = "services-range"
-    ip_cidr_range = "192.168.1.0/24"
+    ip_cidr_range = "10.0.0.0/14"
   }
 
   secondary_ip_range {
     range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.64.0/22"
+    ip_cidr_range = "10.4.0.0/19"
   }
 }
